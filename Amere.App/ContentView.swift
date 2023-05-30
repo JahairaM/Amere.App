e4bdbe8c84be0263cd0c1bc6e4a8e5eac4492a16
Jahaira Maxwell <Maxawell@msu.edu>
2023-05-30 10:41:21 -0400

    blue block added

--- Amere.App/ContentView.swift
+++ Amere.App/ContentView.swift
@@ -7,11 +7,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+    // State variable to hold the list of items/activities
+    
     @State private var isPresentingAddActivity = false
+    // State variable to control the presentation of the AddActivityView sheet
     
     func removeRows(at offsets: IndexSet) {
         items.remove(atOffsets: offsets)
         UserDefaults.standard.setValue(items, forKey: "items")
+        // Function to remove rows at the specified offsets and update the user defaults
     }
     
     func activityDescription(_ activity: String, _ category: String, _ days: Set<String>, _ duration: Int) -> String {
@@ -19,6 +23,7 @@ struct ContentView: View {
         let daysString = days.sorted().joined(separator: ", ")
         description += "\(daysString)\nDuration: \(duration) minutes"
         return description
+        // Function to generate the description of an activity
     }
     
     func categoryColor(_ category: String) -> Color {
@@ -28,34 +33,48 @@ struct ContentView: View {
             "Spirit": Color.yellow
         ]
         return categories[category] ?? .gray
+        // Function to determine the color associated with a category
     }
     
     var body: some View {
-        RingsView()
+        RingsView() // Placeholder for RingsView (not included in the code)
+        
         NavigationView {
             List {
                 ForEach(items, id: \.self) { item in
+                    // Extracting the components from the activity description
                     let components = activityDescription(item, "Category", Set<String>(), 0).components(separatedBy: "\n")
                     let activity = components[0]
                     let category = components[1]
                     let days = components[2]
                     let duration = components[3]
                     
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(activity)
-                            .font(.headline)
-                        Text(days)
-                            .font(.subheadline)
-                        Text(duration)
-                            .font(.subheadline)
+                    HStack(spacing: 8) {
+                        RoundedRectangle(cornerRadius: 8)
+                            .frame(width: 75, height: 75)
+                            .foregroundColor(Color.blue)
+                            .ignoresSafeArea()
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(activity)
+                                .font(.headline)
+                            Text(days)
+                                .font(.subheadline)
+                            Text(duration)
+                                .font(.subheadline)
+                        }
                     }
-                    .padding()
+                    .padding(.vertical, 0)
+                        .padding(.leading, 0)
+                        .frame(width: 300, height: 75)
                     .background(categoryColor(category))
                     .cornerRadius(8)
                     .foregroundColor(.white)
                 }
                 .onDelete(perform: removeRows)
+                // Displaying the list of items/activities with delete functionality
             }
+            .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Amare")
             .navigationBarItems(trailing: Button(action: {
                 isPresentingAddActivity = true
@@ -66,6 +85,7 @@ struct ContentView: View {
                 AddActivityView(isPresented: $isPresentingAddActivity, items: $items)
             })
             .preferredColorScheme(.dark)
+            // Setting the navigation title, add button, and dark color scheme
         }
     }
 }
@@ -93,6 +113,7 @@ struct AddActivityView: View {
             Form {
                 Section(header: Text("Activity")) {
                     TextField("Enter Activity...", text: $newActivity)
+                    // Text field to enter the new activity
                 }
                 
                 Section(header: Text("Category")) {
@@ -116,6 +137,7 @@ struct AddActivityView: View {
                                 }
                             }
                             .buttonStyle(BorderlessButtonStyle())
+                            // Buttons to select the category of the activity
                         }
                     }
                 }
@@ -138,6 +160,7 @@ struct AddActivityView: View {
                                         .clipShape(Circle())
                                 }
                                 .buttonStyle(PlainButtonStyle())
+                                // Buttons to select the days of the week for the activity
                             }
                         }
                         .padding(.horizontal)
@@ -148,6 +171,7 @@ struct AddActivityView: View {
                     Stepper(value: $duration, in: 0...120, step: 15) {
                         Text("\(duration) minutes")
                     }
+                    // Stepper to select the duration of the activity
                 }
                 
                 Section {
@@ -162,10 +186,12 @@ struct AddActivityView: View {
                         Text("Cancel")
                             .foregroundColor(.red)
                     })
+                    // Buttons to save or cancel adding the activity
                 }
             }
             .navigationBarTitle("Add Activity")
             .preferredColorScheme(.dark)
+            // Setting the navigation title and dark color scheme
         }
     }
     
@@ -175,6 +201,7 @@ struct AddActivityView: View {
             UserDefaults.standard.setValue(items, forKey: "items")
         }
         isPresented = false
+        // Function to add the new activity to the list and update user defaults
     }
 }
 
@@ -194,11 +221,11 @@ extension View {
         self.modifier(GlowEffect(color: color, radius: radius))
     }
 }
+// View modifier to apply a glow effect to a view
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
+// Preview provider for ContentView
